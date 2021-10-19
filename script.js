9514ee66ee013842bddce923e1bd3e5cf6cc3bc7
vijayB <88872426+vijay451@users.noreply.github.com>
2021-10-19 10:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const container=document.getElementsByClassName("container")[0];
+container.innerHTML=`<h1>REGISTER WITH US</h1>
+                        <form>
+                        <div class="box">
+                        <label class="details">User Name</label>
+                        <input type="text" class="form" placeholder="Enter your name"><br>
+                        </div>
+                        <div class="box">
+                        <label class="details">Email</label>
+                        <input type="text" class="form" placeholder="Enter your emailID"><br>
+                        </div>
+                        <div class="box">
+                        <label class="details">Password</label>
+                        <input type="text" class="form" placeholder="Enter your password"><br>
+                        </div>
+                        <div class="box">
+                        <label class="details">Confirm Password</label>
+                        <input type="text" class="form" placeholder="Confirm your password"><br>
+                        </div>
+                        <button class="btn">Submit</button>
+                        </form>`
+                        
